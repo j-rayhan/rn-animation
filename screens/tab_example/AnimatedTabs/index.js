a880dcfe5d2aaf3aeebfbca777469ae18ced50f1
johir-rayhan <johir.rayhan01@gmail.com>
2021-02-05 17:23:00 +0600

    Start Animated Tabs

--- screens/tab_example/AnimatedTabs/index.js
+++ screens/tab_example/AnimatedTabs/index.js
@@ -1,24 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
-// const images = {
-//   man:
-//     'https://images.pexels.com/photos/3147528/pexels-photo-3147528.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-//   women:
-//     'https://images.pexels.com/photos/2552130/pexels-photo-2552130.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-//   kids:
-//     'https://images.pexels.com/photos/5080167/pexels-photo-5080167.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-//   skullcandy:
-//     'https://images.pexels.com/photos/5602879/pexels-photo-5602879.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-//   help:
-//     'https://images.pexels.com/photos/2552130/pexels-photo-2552130.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-// };
-// const data = Object.keys(images).map((i) => ({
-//   key: i,
-//   title: i,
-//   image: images[i],
-// }));
+import { StyleSheet, Text, View, Dimensions, FlatList, Animated, Image } from 'react-native';
+
+const { width, height } = Dimensions.get('screen');
+
+const images = {
+  man:
+    'https://images.pexels.com/photos/3147528/pexels-photo-3147528.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  women:
+    'https://images.pexels.com/photos/2552130/pexels-photo-2552130.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  kids:
+    'https://images.pexels.com/photos/5080167/pexels-photo-5080167.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  skullcandy:
+    'https://images.pexels.com/photos/5602879/pexels-photo-5602879.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  help:
+    'https://images.pexels.com/photos/2552130/pexels-photo-2552130.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+};
+const data = Object.keys(images).map((i) => ({
+  key: i,
+  title: i,
+  image: images[i],
+}));
 
 const styles = StyleSheet.create({
   container: {
@@ -30,20 +34,35 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
+  const [loaded] = useFonts({
+    Menlo: require('../../../assets/fonts/Menlo-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
       <StatusBar hidden />
-      <Text style={{ fontSize: 42 }}>❤️</Text>
-      <Text
-        style={{
-          fontFamily: 'Menlo',
-          marginTop: 10,
-          fontWeight: '800',
-          fontSize: 16
+
+      <Animated.FlatList
+        data={data}
+        keyExtractor={item => item.key}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        pagingEnabled
+        renderItem={({ item }) => {
+          return (
+            <View style={{ width, height}}>
+              <Image
+                source={{ uri: item.image }}
+                style={{ flex: 1, resizeMode: 'cover' }}
+              />
+              <View style={[StyleSheet.absoluteFillObject, {backgroundColor: 'rgba(0,0,0,0.3)'}]} />
+            </View>
+          )
         }}
-      >
-        Expo
-      </Text>
+      />
       <Text style={{ fontFamily: 'Menlo', fontStyle: 'italic', fontSize: 12 }}>
         (expo.io)
       </Text>
