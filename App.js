a7d1e3058f72a060b8f9bd9fad6c4a7581484855
johir-rayhan <johir.rayhan01@gmail.com>
2021-02-13 20:07:47 +0600

    Start bottom tabs

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import 'react-native-gesture-handler';
 
 import DrawerTABS from './src/Navigation/Drawer';
+import BottomTABS from './src/Navigation/Tabs';
 
 export default function App() {
   return (
-    <DrawerTABS />
+    <BottomTABS />
   )
 }
 
