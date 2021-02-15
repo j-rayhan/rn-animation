30a30b3ab859268ecd9016dcb6c694734d95457e
j-rayhan <johir.rayhan01@gmail.com>
2021-02-15 09:35:06 +0600

    Initial commit

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,33 @@
+// Inspiration: https://dribbble.com/shots/14154226-Rolodex-Scrolling-Animation/attachments/5780833?mode=media
+// Photo by Sharefaith from Pexels
+// Background image: https://www.pexels.com/photo/pink-rose-closeup-photography-1231265/
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StatusBar, FlatList, Image, Animated, Text, View, Dimensions, StyleSheet, TouchableOpacity, Easing, SafeAreaViewBase, SafeAreaView } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+// import { Text, View } from '../components/Themed';
+import faker from 'faker';
+
+const { width, height } = Dimensions.get('screen');
+
+faker.seed(10);
+const DATA = [...Array(30).keys()].map((_, i) => {
+    return {
+        key: faker.random.uuid(),
+        image: `https://randomuser.me/api/portraits/${faker.helpers.randomize(['women', 'men'])}/${faker.random.number(60)}.jpg`,
+        name: faker.name.findName(),
+        jobTitle: faker.name.jobTitle(),
+        email: faker.internet.email(),
+    };
+});
+
+const SPACING = 20;
+const AVATAR_SIZE = 70;
+
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+    <StatusBar hidden/>
     </View>
   );
 }
@@ -30,3 +48,8 @@ const styles = StyleSheet.create({
     width: '80%',
   },
 });
+// export default () => {
+//     return <View style={{flex: 1, backgroundColor: '#fff'}}>
+//         <StatusBar hidden/>
+//     </View>
+// }
